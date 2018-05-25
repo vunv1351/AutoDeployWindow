4c40a621f6d91c33aa44c8fe8e23f688ba1e2c5d
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-25 17:12:39 +0700

    edit yml and run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-call echo "build"
+call echo "run"
 call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"  "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeploywindow.sln"
-call "build ok"
+call "run ok"
 pause
