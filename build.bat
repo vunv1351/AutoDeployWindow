a950bf430fa40fe8795511862d9a9f7ab93f812d
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-25 17:07:38 +0700

    add file

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ call echo " tạo forel"
 call "mkdir C:\Users\Administrator\Desktop\AutoDeploywindow"
 call echo "delete"
 call "C:\Users\Administrator\Desktop\AutoDeploywindow\*.*/s /q
+call "rmdir C:\Users\Administrator\Desktop\AutoDeploywindow /s /q
 call echo "build"
 call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"  "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeploywindow.sln"
 call "build ok"
