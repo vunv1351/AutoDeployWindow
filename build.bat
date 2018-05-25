f597859d4674481d578cfd024607fc4e4a24d683
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-25 16:54:32 +0700

    edit .bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+call echo " tạo forel"
+call "mkdir C:\Users\Administrator\Desktop\AutoDeploywindow"
 call echo "delete"
 call "C:\Users\Administrator\Desktop\AutoDeploywindow\*.*/s /q
 call echo "build"
