3ae615a6c6b2bc6308b9a7b1bc74b383742d4979
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-25 15:17:51 +0700

    bat'

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call echo "build"
-call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" "C:\Users\Administrator\Desktop\AutoDeployWindow\AutoDeployWindow.sln"
+call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" 
 call "build ok"
 pause
