60db9c9737fc9ce0449f1e83126a339e89d1542f
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-25 17:21:35 +0700

    update

--- build.bat
+++ build.bat
@@ -6,4 +6,3 @@ call "rmdir C:\Users\Administrator\Desktop\AutoDeploywindow /s /q
 call echo "build"
 call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"  "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeploywindow.sln"
 call "build ok"
-pause
