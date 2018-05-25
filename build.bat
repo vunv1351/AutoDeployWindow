2fbbaf66d3f78f61bd7a60638e6e8f0a8663c71a
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-25 16:48:06 +0700

    edit bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+call echo "delete"
+call "C:\Users\Administrator\Desktop\AutoDeploywindow\*.*/s /q
 call echo "build"
 call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"  "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeploywindow.sln"
 call "build ok"
