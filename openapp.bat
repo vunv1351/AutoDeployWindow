18802350bc0c6a5b9eb5658e50d1cbf1d602e3ea
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-29 10:28:07 +0700

    add openapp

---
+++ openapp.bat
@@ -0,0 +1,4 @@
+call echo "run application"
+call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"  "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeployWindow\bin\Debug\AutoDeployWindow.exe"
+call "run application ok"
+pause
