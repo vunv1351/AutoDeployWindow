8fe02f86448b1ed0d1cdaadeb81a55dd5f8071ab
DESKTOP-71PGQU1\vNet <nguyenvu19995>
2018-05-29 13:38:11 +0700

    edit yml

--- openapp.bat
+++ openapp.bat
@@ -1,4 +1,4 @@
 call echo "run application"
-call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"  "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeployWindow\bin\Debug\AutoDeployWindow.exe"
+call "C:\Users\Administrator\Desktop\AutoDeploywindow\AutoDeployWindow\bin\Debug\AutoDeployWindow.exe"
 call "run application ok"
 pause
